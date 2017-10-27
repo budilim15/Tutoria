8347adc7cab4b7e0e88a97ea0e3e3d25a8702df0
budiman <budiman_1597@hotmail.com>
2017-10-28 04:05:04 +0800

    V 1.2
    Making a models for users

--- Tutoria/users/admin.py
+++ Tutoria/users/admin.py
@@ -1,1 +1,9 @@
 from django.contrib import admin
+from users.models import UserProfile,Student, Tutor, BookingRecord, BlackOut, Timeslot
+
+admin.site.register(UserProfile)
+admin.site.register(Student)
+admin.site.register(Tutor)
+admin.site.register(BookingRecord)
+admin.site.register(BlackOut)
+admin.site.register(Timeslot)
