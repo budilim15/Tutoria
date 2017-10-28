860ea75d57ec748c9d8b43189845c1719395910c
budiman <budiman_1597@hotmail.com>
2017-10-28 10:49:28 +0800

    ugh

--- Tutoria/tutoria/main/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    return render(request,'main/home.html')
-
-def login(request):
-    return render(request,'main/login.html')
--- Tutoria/tutoria/users/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from users.models import Student
-from users.models import Tutor
-from users.models import BookingRecord
-
-
-admin.site.register(Student)
-admin.site.register(Tutor)
-admin.site.register(BookingRecord)
